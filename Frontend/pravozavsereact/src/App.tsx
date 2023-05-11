f2568f547fc84e0c488ccaa06eb299fb425f331e
LahT <teo.lah@student.um.si>
2023-05-11 16:05:30 +0200

    Dodana knjiznica PrimeReact #PVZ-5

--- Frontend/pravozavsereact/src/App.tsx
+++ Frontend/pravozavsereact/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";     
 import Greetings from './Components/Greetings/Greetings';
 import TestUseOfAtoms from './Components/TestUseOfAtoms/TestUseOfAtom';
 
--- Frontend/pravozavsereact/src/Components/TestUseOfAtoms/TestUseOfAtom.tsx
+++ Frontend/pravozavsereact/src/Components/TestUseOfAtoms/TestUseOfAtom.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAtom } from "jotai";
 import { test1Atom, test2Atom } from "../../Atoms/TestsAtoms";
+import { Button } from 'primereact/button'
 
 export default function TestUseOfAtoms(): JSX.Element {
   const [test1, setTest1] = useAtom(test1Atom); //To je cisto isto kot useState(), samo da useAtom() vzame in ureja stanje, ki je narejeno nekje drugje
@@ -9,13 +10,13 @@ export default function TestUseOfAtoms(): JSX.Element {
   return (
     <>
       <p>Testni atom 'test1' ima vrednost: {test1}</p>
-      <button onClick={ ():void => setTest1(test1+1) }>Povečaj številko 'test1'</button>
+      <Button onClick={ ():void => setTest1(test1+1) }>Povečaj številko 'test1'</Button>
 
       <br />
       <p>Testni atom 'test2' je objekt: {test2.name} {test2.years}</p>
-      <button onClick={ ():void => setTest2({name: 'Janez', years:38}) }>Spremeni ime na Janez</button>
-      <button onClick={ ():void => setTest2({...test2, years: 5}) }>Zmanšaj leta na 5</button>
-      <button onClick={ ():void => setTest2((before) => { return {...before, name: 'Aleks'} }) }>Spremeni ime na Aleks</button>
+      <Button onClick={ ():void => setTest2({name: 'Janez', years:38}) }>Spremeni ime na Janez</Button>
+      <Button onClick={ ():void => setTest2({...test2, years: 5}) }>Zmanšaj leta na 5</Button>
+      <Button onClick={ ():void => setTest2((before) => { return {...before, name: 'Aleks'} }) }>Spremeni ime na Aleks</Button>
     </>
     //Zadnji gumb je najboljsi nacin za spreminjanje stanja    parameter before je stanje tik pred spremembo
   );
