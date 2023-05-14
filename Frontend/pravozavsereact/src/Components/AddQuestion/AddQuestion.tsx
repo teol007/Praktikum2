d7d8a2ca890e3944501f171e4e68b0cd16cac6d2
maja <maja.prosenjak@student.um.si>
2023-05-14 22:03:47 +0200

    spremembe vprasalnika

--- Frontend/pravozavsereact/src/Components/AddQuestion/AddQuestion.tsx
+++ Frontend/pravozavsereact/src/Components/AddQuestion/AddQuestion.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { InputText } from "primereact/inputtext";
 
 interface AddQuestionProps{
 
@@ -75,6 +76,16 @@ export default function AddQuestion(): JSX.Element {
                 Pošlji
             </Button> <br />
         </Form>
+
+        <form>
+        <div className="flex flex-column gap-2">
+            <label htmlFor="username">Username</label>
+            <InputText id="username" aria-describedby="username-help" />
+            <small id="username-help">
+                Enter your username to reset your password.
+            </small>
+        </div>
+        </form>
     </div>
   );
 }
